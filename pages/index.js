af39d1ea7a48929ea75c252f3073d9bec1b40aec
Tal Waserman <twaserman@ebay.com>
2021-02-13 07:18:41 +0200

    add leng

--- pages/index.js
+++ pages/index.js
@@ -51,12 +51,12 @@ export default function Home({ allPostsData }) {
 
         <div className="grid">
           <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
+            <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
+            <h2>Learn &rarr;</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
@@ -64,7 +64,7 @@ export default function Home({ allPostsData }) {
             href="https://github.com/zeit/next.js/tree/master/examples"
             className="card"
           >
-            <h3>Examples &rarr;</h3>
+            <h2>Examples &rarr;</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
@@ -72,7 +72,7 @@ export default function Home({ allPostsData }) {
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className="card"
           >
-            <h3>Deploy &rarr;</h3>
+            <h2>Deploy &rarr;</h2>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
@@ -103,7 +103,7 @@ export default function Home({ allPostsData }) {
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+          <img src="/vercel.svg" alt="Vercel Logo" className="logo" style={{width: '100', height: '100'}} />
         </a>
       </footer>
 
@@ -215,7 +215,7 @@ export default function Home({ allPostsData }) {
           border-color: #0070f3;
         }
 
-        .card h3 {
+        .card h2 {
           margin: 0 0 1rem 0;
           font-size: 1.5rem;
         }
